9d3875ff1717cfd5461ef93f0c6324c1ded3a440
gaurav <malikgaurav080@gmail.com>
2020-05-10 10:29:19 +0530

    second

--- my.php
+++ my.php
@@ -1,3 +1,3 @@
 <?php
-print ""[Dgaurav malik[D[D[D[M[D[D[D[D[D[D[G[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-?>[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D" ;[B[B[B[D[D[D[D
+print   "Gaurav Malik"
+?>
